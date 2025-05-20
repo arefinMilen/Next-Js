866c3d987394a66cf4c2ba2733e40e4712044e41
arefinMilen <arefin15-5279@diu.edu.bd>
2025-05-20 23:59:32 +0600

    loading meta tag

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,10 +1,12 @@
 
 import React from 'react'
 import getPost  from '@/lib/getPost';
+import { getPostComment } from '@/lib/getPostComment';
 
 export async function generateMetadata({params}) {
   const {id} = params;
   const post = await getPost(id);
+  const comments = await getPostComment(id);
   return {
     title: post.title,
     description: post.body,
@@ -13,11 +15,25 @@ export async function generateMetadata({params}) {
 
 export default async function PostPage({params}) {
     const {id} = params;
-    const post = await getPost(id);
+    const postPromise = getPost(id);
+    const commentsPromise = getPostComment(id);
+
+    const [post , comments] = await Promise.all([postPromise, commentsPromise]);
   return (
     <div className='mt-6'>
       <h2 className="text-blue-500">{post.title}</h2>
       <p className="mt-3">{post.body}</p>
+      <hr />
+      <div className="mt-10">
+        <h1>Comments</h1>
+        <ul>
+          {comments.map((comment)=> (
+            <li className='mb-3' key={comment.id}>{comment.body}
+            </li>
+          ))}
+        </ul>
+
+      </div>
     </div>
   )
 }
