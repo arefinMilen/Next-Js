e39649a6c85731fef12a2138fe182aa82c9d100e
arefinMilen <arefin15-5279@diu.edu.bd>
2025-08-31 23:59:12 +0600

    update dynamics posts

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -2,38 +2,35 @@
 import React from 'react'
 import getPost  from '@/lib/getPost';
 import { getPostComment } from '@/lib/getPostComment';
+import Head from 'next/head';
 
 export async function generateMetadata({params}) {
   const {id} = params;
   const post = await getPost(id);
-  const comments = await getPostComment(id);
+
   return {
     title: post.title,
     description: post.body,
   };
 }
 
-export default async function PostPage({params}) {
+export default async function PostPage({Promise}) {
     const {id} = params;
     const postPromise = getPost(id);
     const commentsPromise = getPostComment(id);
 
-    const [post , comments] = await Promise.all([postPromise, commentsPromise]);
+    // const [post , comments] = await Promise.all([postPromise, commentsPromise]);
+    const post = await Promise;
   return (
     <div className='mt-6'>
       <h2 className="text-blue-500">{post.title}</h2>
       <p className="mt-3">{post.body}</p>
       <hr />
-      <div className="mt-10">
-        <h1>Comments</h1>
-        <ul>
-          {comments.map((comment)=> (
-            <li className='mb-3' key={comment.id}>{comment.body}
-            </li>
-          ))}
-        </ul>
-
-      </div>
+      <Suspense fallback="<h1>Loading comments...</h1>" >
+        <Comments  promise={commentsPromise} />
+       
+      </Suspense>
+      
     </div>
   )
 }
